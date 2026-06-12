4705fe7e92e4999539de77025cecf21344960087
Kimheng-SOK <sokkimheng008@gmail.com>
2026-06-12 09:32:27 +0700

    chore(main): remove temporary backend test function

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,26 +1,9 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-Future<void> testBackend() async {
-  try {
-    final response = await http.get(
-      Uri.parse(
-        '${dotenv.get('API_BASE_URL', fallback: 'http://localhost:3000')}/auth/health',
-      ),
-    );
-
-    print('Status: ${response.statusCode}');
-    print('Body: ${response.body}');
-  } catch (e) {
-    print('ERROR: $e');
-  }
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
-  await testBackend();
   runApp(const ResQApp());
 }
